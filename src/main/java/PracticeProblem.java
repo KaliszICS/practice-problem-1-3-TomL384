327b92525ded064b74b11bc8761d4973ce58e96e
TomL384 <350286454@tdsb.ca>
2026-02-19 15:39:51 +0000

    final

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -9,18 +9,36 @@ public class PracticeProblem {
 
 	public static void q1() {
 		//Write question 1 code here
+		System.out.println(" * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n");
+		System.out.println("* * * * *\n*       *\n*       *\n*       *\n* * * * *\n");
+		System.out.println("* * * * * *\n *       *\n  *     *\n   *   *\n    * *\n     *");
 	}
 
 	public static void q2() {
 		//Write question 2 code here
+		String v;
+		v ="          '__'" +"\n          (oo)" + "\n  /========//" + "\n / || XX ||" + "\n*  ||----||" + "\n   VV    VV" + "\n   ''    ''" ;
+		System.out.println(v);
 	}
 
 	public static void q3() {
 		//Write question 3 code here
+		String a;
+		String b;
+		a = "Fortnite " ;
+		b = "is awful" ;
+		System.out.print(a);
+		System.out.println(b);
 	}
+		
 
 	public static void q4() {
 		//Write question 4 code here
+		String a;
+		String b;
+		a = "Don't " ;
+		b = "@ me" ;
+		System.out.println(a + b);
 	}
 
 
